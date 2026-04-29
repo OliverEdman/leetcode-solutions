4a0738b8a56a115b15cd0def044a190815dd48e6
Oliver Edman Chrantz <o.edman@icloud.com>
2026-04-29 02:24:35 +0200

    added 191-number-of-1-bits

---
+++ Bit-Manipulation/191-number-of-1-bits/solution.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+int hammingWeight(uint32_t n) {
+    int set_bits = 0;
+
+    while (n > 0) {
+        if ((n & 1) == 1) {
+            set_bits++;
+        }
+
+        n = n >> 1;
+    }
+    return set_bits;
+}
